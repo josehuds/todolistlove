625be2065f74fae7d95b6c6ee5456dac1bf40093
hudson <hudsonribeiro2604@gmail.com[200~  git config --global user.email you@example.com~ git config --global user.email hudsonribeiro2604@gmail.com>
2023-11-09 12:09:03 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/todolistlove/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todolistlove
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/api.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class name extends StatelessWidget {
+  const name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
