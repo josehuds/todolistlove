369aeb1cb53bbabf45add1bfe3af9af8f3c1574d
hudson <hudsonribeiro2604@gmail.com[200~  git config --global user.email you@example.com~ git config --global user.email hudsonribeiro2604@gmail.com>
2023-11-13 11:08:47 -0300

    salvando imagem

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -1,10 +1,184 @@
 import 'package:flutter/material.dart';
+import 'package:todolistlove/view/register/register.dart';
 
-class name extends StatelessWidget {
-  const name({super.key});
+class Login extends StatelessWidget {
+  const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+  
+          color: Colors.purpleAccent.shade200,
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Padding(padding: EdgeInsets.only(top: 20.0)),
+                Card(
+                  color: Colors.white,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(200.0),
+                      bottomRight: Radius.circular(200.0),
+                    ),
+                  ),
+                  elevation: 0,
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        'Lovepeople_logo.jpg',
+                        height: 150,
+                        width: double.infinity,
+                        fit: BoxFit.contain,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          'Lovepeople',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                const Text(
+                  'Que bom que voltou! ',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            hintText: 'Numero de telefone, email ou CPF',
+                            hintStyle: const TextStyle(
+                              color: Colors.blue,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 2.0),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, insira seu nome.';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          obscureText: true,
+                          cursorColor: Colors.blue.shade900,
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                              suffixIcon: const Icon(
+                                Icons.visibility,
+                                color: Colors.blue,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              hintText: 'Senha',
+                              hintStyle: const TextStyle(
+                                color: Colors.blue,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: const BorderSide(
+                                      color: Colors.transparent, width: 2.0))),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, insira seu nome.';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.blue.shade900),
+                              padding: const MaterialStatePropertyAll(
+                                  EdgeInsets.symmetric(horizontal: 20))),
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 90,
+                        ),
+                        const Text(
+                          '-----------------------------------------------------------------------------------------',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: ((context) => Register()),
+                                ),
+                              );
+                            },
+                            child: const Align(
+                                alignment: Alignment.center,
+                                child: Text('Nao possui cadastro? Clique aqui',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 236, 133, 36),
+                                    ))))
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
